f06ff34100e59d95acffbffd78a73a6db3474620
danielr0d <daniel.rodrigues.98@hotmail.com>
2025-11-18 22:38:44 -0300

    std prnt

---
+++ 3/print.rs
@@ -0,0 +1,11 @@
+fn main() {
+
+    println!("{} days", 31);
+
+    println!("{0}, this is {1}. {1}, this is {0}", "Danieo", "Biroliro");
+
+    println!("{subject} {verb} {object}",
+             object="the lazy dog",
+             subject="the quick brown fox",
+             verb="jumps over");
+}
