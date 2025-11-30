c9beb265cb1fec21da81e91676536e4809039ac8
danielr0d <daniel.rodrigues.98@hotmail.com>
2025-11-30 18:43:34 -0300

    fix typo yaaaaaaaaaaaay

--- 1/1.2/1.2.3/formatting.rs
+++ 1/1.2/1.2.3/formatting.rs
@@ -8,7 +8,7 @@ struct City {
 
 impl Display for City {
     fn fmt (&self, f: &mut Formatter) -> fmt::Result {
-        let lac_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
         let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' }; 
 
         write!(f, "{}: {:.3}°{} {:.3}°{}",
@@ -32,7 +32,7 @@ fn main() {
         println!("{}", city);
     }
     for color in [
-        Color { red: 128, green: 255, blue: 90},
+        Color { red: 128, green: 255, blue: 90 },
         Color { red: 0, green: 3, blue: 254 },
         Color { red: 0, green: 0, blue: 0 },
     ] {
