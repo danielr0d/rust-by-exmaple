95788dc11284b2211f8d276897769860cc45d520
danielr0d <daniel.rodrigues.98@hotmail.com>
2025-12-11 12:51:12 -0300

    some tuples stuff yay

---
+++ 2/2.2/tuples.rs
@@ -0,0 +1,23 @@
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    let (int_param, bool_param) = pair;
+
+    (bool_param, int_param)
+}
+
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+fn main() {
+    let long_tuple = (1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true);
+   
+    println!("Long tuple first value: {}", long_tuple.0);
+    println!("Long tuple second value: {}", long_tuple.1);
+
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+    //let fucking_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
+    //println!("This shit is too long: {:?}", fucking_long_tuple);
+    
+}
