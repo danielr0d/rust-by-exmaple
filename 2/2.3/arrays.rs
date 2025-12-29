d645263b8bff663bb2514e75958f91141d8625e9
danielr0d <daniel.rodrigues.98@hotmail.com>
2025-12-29 00:11:39 -0300

    fkg typo

--- 2/2.3/arrays.rs
+++ 2/2.3/arrays.rs
@@ -1,6 +1,6 @@
 use std::mem;
 
 fn analyze_slice(slice: &[i32]) {
-    println!("First element of slice: {}", slice[0]);\
+    println!("First element of slice: {}", slice[0]);
     println!("The slice has {} elements", slice.len());
 }
