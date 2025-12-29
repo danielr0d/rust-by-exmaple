e5f0e870b7404309de51557797fe3b4201dde25f
danielr0d <daniel.rodrigues.98@hotmail.com>
2025-12-29 00:18:21 -0300

    add some array shit

--- 2/2.3/arrays.rs
+++ 2/2.3/arrays.rs
@@ -4,3 +4,24 @@ fn analyze_slice(slice: &[i32]) {
     println!("First element of slice: {}", slice[0]);
     println!("The slice has {} elements", slice.len());
 }
+
+fn main() {
+
+  let xs: [i32; 5] = [1, 2, 3, 4, 5]
+
+  let ys: [i32; 500] = [0; 500];
+
+  println!("First element of the array; {}", xs[0]);
+  println!("Second element of the array: {}", xs[1]);
+
+  println!("Number of elements in array: {}", xs.len());
+  
+  println!("Array occupies {}", mem::size_of_val(&xs));
+
+  println!("Borrow the whole array as a slice.")
+  analyze_slice(&xs);
+
+  println!("Borrow a section of the array as a slice.");
+  analyze_slice(ys[1..4]);
+
+  }
