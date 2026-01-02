6a87ac7f53be03029eaa1895222950af51b07c89
danielr0d <daniel.rodrigues.98@hotmail.com>
2026-01-02 12:26:13 -0300

    more array shit

--- 2/2.3/arrays.rs
+++ 2/2.3/arrays.rs
@@ -7,21 +7,35 @@ fn analyze_slice(slice: &[i32]) {
 
 fn main() {
 
-  let xs: [i32; 5] = [1, 2, 3, 4, 5]
+    let xs: [i32; 5] = [1, 2, 3, 4, 5]
 
-  let ys: [i32; 500] = [0; 500];
+    let ys: [i32; 500] = [0; 500];
 
-  println!("First element of the array; {}", xs[0]);
-  println!("Second element of the array: {}", xs[1]);
+    println!("First element of the array; {}", xs[0]);
+    println!("Second element of the array: {}", xs[1]);
 
-  println!("Number of elements in array: {}", xs.len());
+    println!("Number of elements in array: {}", xs.len());
   
-  println!("Array occupies {}", mem::size_of_val(&xs));
+    println!("Array occupies {}", mem::size_of_val(&xs));
+    
+    println!("Borrow the whole array as a slice.")
+    analyze_slice(&xs);
+    
+    println!("Borrow a section of the array as a slice.");
+    analyze_slice(&ys [1 .. 4]);
 
-  println!("Borrow the whole array as a slice.")
-  analyze_slice(&xs);
+    let empty_array: [u32; 0] = [];
+    assert_eq!(&empty_array, &[]);
+    assert_eq!(&empty_array, &[][..]);
 
-  println!("Borrow a section of the array as a slice.");
-  analyze_slice(ys[1..4]);
+    for i in 0..xs.len() + 1 {
+        match xs.get(i) {
+            Some(xval) => println!("{}: {}", xval),
+            None => println!("Slow down! {} is too far!", i),
+        }
+    }
+  
+    println!("{}", xs[5]);
+    println!("{}", xs[..][5]);
 
   }
