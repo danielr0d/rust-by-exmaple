ff80b590571432250b5f431e9d24706b08e39462
danielr0d <daniel.rodrigues.98@hotmail.com>
2026-01-05 15:52:14 -0300

    structure stff

--- 3/3.1/structures.rs
+++ 3/3.1/structures.rs
@@ -28,6 +28,29 @@ fn main() {
     println!({:?}, peter);
 
     let point: Point = Point  { x: 5.2; y: 0.4 };
-    let another_point: Point = Point { x: 10.3; y:0.2};
+    let another_point: Point = Point { x: 10.3; y:0.2 };
+
+    println!("point coordinates: ({}, {})", point.x, point.y);
+
+    let bottom_right = Point { x: 10.3, ..another_point };
+
+    println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
+
+    let Point { x: left_edge, y: top_edge } = point;
+
+    let _rectangle = Rectangle {
+        top_left: Point { x: left_edge, y: top_edge },
+        bottom_right: bottom_right,
+    }
+
+    let _unit = Unit;
+
+    let pair = Pair(1, 0.1);
+
+    println!("pair contains {:?} amd {:?}", pair.0, pair.1);
+
+    let Pair(integer, decimal) = pair;
+
+    println!("pair contains {:?} and {:?}", integer, decimal);
 
 }
