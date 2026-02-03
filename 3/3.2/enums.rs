eebfed0e58a8260791d9c0e70e9fa2a41adf7a34
danielr0d <daniel.rodrigues.98@hotmail.com>
2026-02-02 23:22:04 -0300

    enum type shit

---
+++ 3/3.2/enums.rs
@@ -0,0 +1,12 @@
+enum WebEvent {
+    PageLoad,
+    PageUnload,
+    KeyPress(char),
+    Paste(String),
+    Click { x: i64, y: i64 };
+}
+
+fn inspect (event: WebEvent) {
+    match event {
+    }
+}
