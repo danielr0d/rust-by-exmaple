b477ee744915e5de880c37bc2f1a54ca636b3d94
danielr0d <daniel.rodrigues.98@hotmail.com>
2026-02-04 19:03:39 -0300

    use

---
+++ 3/3.2/3.2.1/use.rs
@@ -0,0 +1,31 @@
+#![allow(dead_code)]
+
+enum Stage {
+    Beginner,
+    Advanced,
+}
+
+enum Role {
+    Student,
+    Teacher
+}
+
+fn main() {
+
+    use Stage::{Beginner, Advanced};
+    use Role::*
+
+    let stage = Beginner;
+    let role = Student;
+
+    match stage {
+        Beginner => println!("Beginners are starting their learning nightmare!");
+        Advanced => println!("Advanced learners are masterging their fears...");
+    }
+
+    match role {
+        Student => println!("Students are acquiring knowledge!");
+        Teacher => println!("Teachers are spreading knowledge!");
+    }
+
+}
