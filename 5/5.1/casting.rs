7225eab749b005ece19e8b9714674e3948e223aa
danielr0d <daniel.rodrigues.98@hotmail.com>
2026-02-24 14:58:38 -0300

    more castings

--- 5/5.1/casting.rs
+++ 5/5.1/casting.rs
@@ -10,9 +10,39 @@ fn main() {
 
     let character = decimal as char;
 
-    print!("Casting: {} -> {} -> {}", decimal, integer, character);
+    println!!("Casting: {} -> {} -> {}", decimal, integer, character);
 
-    print!("1000 as a u16 is: {}", 1000 as u16);
+    println!("1000 as a u16 is : {}", 1000 as u16);
+
+    println!("1000 as a u8 is : {}", 1000 as u8);
+
+    println!(" -1 as u8 is : {}", (-1i8) as u8);
+
+    println!("1000 mod 256 is : {}", 1000 % 256);
+
+    println!(" 128 as a i16 is : {}", 128 as i16);
+
+    println!(" 128 as a i8 is : {}", 128 as i8);
+
+    println!("1000 as a u8 is : {}", 100 as u8);
+
+    println!(" 232 as a i8 is : {}", 232 as i8);
+
+    println!(" 300.0 as u8 is : {}", 300.0_f32 as u8);
+
+    println!("-100.0 as u8 is : {}", -100.0_f32 as u8);
+
+    println!(" nan as u8 is : {}", f32::NAN as u8);
+
+    unsafe {
+        println!(" 300.0 as u8 is : {}", 300.0_f32.to_int_unchecked::<u8>());
+
+        println!("-100.0 as u8 is : {}", (-100.0_f32).to_int_unchecked::<u8>());
+
+        println!(" nan as u8 is : {}", f32::NAN.to_int_unchecked::<u8>());
+    }
+
+
 
 }
 
