1e9b0f2966e6d520124879a1aee25f6f989b82a9
danielr0d <daniel.rodrigues.98@hotmail.com>
2026-04-05 23:50:45 -0300

    match struct

---
+++ 8/8.5/8.5.1/8.5.1.5/match_struct.rs
@@ -0,0 +1,27 @@
+fn main() {
+    struct Foo {
+        x: (u32, u32),
+        y: u32,
+    }
+
+    let foo {
+        Foo { x: (1, b), y } => println!("first of x is 1, b = {}, y = {} ", b, y),
+
+        Foo { y: 2, x: i } => println!("y is 2, i = {:?}", i),
+
+        Foo { y, .. } => println!("y = {},  we don't care about x", y),
+    }
+
+    let faa = Foo { x: (1, 2), y: 3 };
+
+    let Foo {x : x0, y: y0 } = faa;
+    println!("outside: x0 = {x0:?}, y0 = {y0}");
+
+    struct Bar {
+        foo: Foo,
+    }
+
+    let bar = Bar { foo: faa };
+    let Bar { foo: Foo { x: nested_x, y: nested_y } } = bar;
+    println!("nested: nested_x = {nested_x:?}, nested_y = {nested_y:?}");
+}
