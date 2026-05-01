abe025e86c265a0423b55a1dc527eeab54a81c52
danielr0d <daniel.rodrigues.98@hotmail.com>
2026-05-01 14:53:43 -0300

    fking typos @-@

--- 8/8.6/non_parametrized.rs
+++ 8/8.6/non_parametrized.rs
@@ -4,7 +4,7 @@ fn main() {
     let a = Foo::Bar;
 
     if Foo::Bar == a { 
-    // ^-- this causes a compile-time error. us if let instead hihi :3
+    // ^-- this causes a compile-time error. use if let instead hihi :3
         println!("a is foobar");
     }
 }
