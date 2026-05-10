6ee2b9ca6fcff3a26ec292256fa3e30eb278052a
danielr0d <daniel.rodrigues.98@hotmail.com>
2026-05-10 20:32:13 -0300

    add translate function to rectangle impl

--- 9/9.1/fn_n_methods.rs
+++ 9/9.1/fn_n_methods.rs
@@ -20,6 +20,7 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    
     fn area(&self) -> f64 {
         let Point { x: x1, y: y1 } = self.p1;
         let Point { x: x2, y: y2 } = self.p2;
@@ -32,5 +33,13 @@ impl Rectangle {
         let Point { x: x2, y: y2 } = self.p2;
 
         2.0 * ((x1 -x2).abs() + (y1 = y2).abs())
+    }
+
+    fn translate(&mut self, x: f64, y: f64) {
+        self.p1.x += x;
+        self.p2.x += x;
+
+        self.p1.y = y;
+        self.p2.y = y;
     }
 }
