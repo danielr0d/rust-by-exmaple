8bde6dca61fbd1ffc01d80134187fe8491be995f
danielr0d <daniel.rodrigues.98@hotmail.com>
2026-05-10 20:21:53 -0300

    add reactangle funcionts :3

---
+++ 9/9.1/fn_n_methods.rs
@@ -0,0 +1,36 @@
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+impl Point {
+    
+    fn origin() -> Point {
+        Point { x: 0.0, y: 0.0 }
+    }
+
+    fn new(x: f64, y: f64) -> Point {
+        Point { x: x, y: y}
+    }
+}
+
+struct Rectangle {
+    p1: Point,
+    p2: Point,
+}
+
+impl Rectangle {
+    fn area(&self) -> f64 {
+        let Point { x: x1, y: y1 } = self.p1;
+        let Point { x: x2, y: y2 } = self.p2;
+
+        ((x1 - x2) * (y1 - y2)).abs()
+    }
+
+    fn permiter(&self) -> f64 {
+        let Point { x: x1, y: y1 } = self.p1;
+        let Point { x: x2, y: y2 } = self.p2;
+
+        2.0 * ((x1 -x2).abs() + (y1 = y2).abs())
+    }
+}
