c868d3c21b59143182cf368b79cf5cbaf80a1c44
danielr0d <daniel.rodrigues.98@hotmail.com>
2026-05-11 19:53:21 -0300

    add square n destroy functions

--- 9/9.1/fn_n_methods.rs
+++ 9/9.1/fn_n_methods.rs
@@ -28,11 +28,11 @@ impl Rectangle {
         ((x1 - x2) * (y1 - y2)).abs()
     }
 
-    fn permiter(&self) -> f64 {
+    fn perimeter(&self) -> f64 {
         let Point { x: x1, y: y1 } = self.p1;
         let Point { x: x2, y: y2 } = self.p2;
 
-        2.0 * ((x1 -x2).abs() + (y1 = y2).abs())
+        2.0 * ((x1 -x2).abs() + (y1 - y2).abs())
     }
 
     fn translate(&mut self, x: f64, y: f64) {
@@ -43,3 +43,36 @@ impl Rectangle {
         self.p2.y = y;
     }
 }
+
+struct Pair(Box<i32>, Box<i32>);
+
+impl Pair {
+    fn destroy(self) {
+        let Pair(first, second) = self;
+
+        println!("destroying pair ({}, {})", first, second);
+    }
+}
+
+fn main() {
+    let rectangle = Rectangle {
+        p1: Point::origin(),
+        p2: Point::new(3.0, 4.0),
+    };
+
+    println!("rectangle perimeter: {}", rectangle.perimeter());
+    println!("rectangle area: {}", rectangle.area());
+
+    let mut square = Rectangle {
+        p1: Point::origin(),
+        p2: Point::new(1.0, 1.0),
+    };
+
+    square.translate(1.0, 1.0);
+
+    let pair = Pair(Box::new(1), Box::new(2));
+
+    pair.destroy();
+}
+
+
